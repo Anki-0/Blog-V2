e2c9393c990387b606469e770569808d39b011e6
Ankit Singh <singhankit25apr@gmail.com>
2021-07-21 21:29:43 +0530

    Singn up component refurbished

---
+++ src/Layout/Button/Btn.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useAuthModelToggleValue } from '../../Context/AuthModelToggleContext';
+
+interface props {
+  name: string;
+  className: string;
+  type: string;
+}
+
+const Btn = styled.button`
+  padding: 1rem 2rem;
+  border: none;
+  background: ${({ theme }) => theme.colors.light.blue};
+  color: ${({ theme }) => theme.colors.light.wh};
+
+  font-size: 1.4rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  font-weight: 500;
+  transition: color 0.1s ease-in-out, background 0.2s ease-in-out;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.light.wh};
+    background: ${({ theme }) => theme.colors.light.bluemd};
+  }
+`;
+
+export default function Button({ name, className, type }: props): JSX.Element {
+  const { showAuthModel, setShowAuthModel } = useAuthModelToggleValue();
+
+  const setter = {
+    SignInModel: type === 'loginBtn' ? !showAuthModel.SignInModel : showAuthModel.SignInModel,
+    SignUpModel: type === 'signupBtn' ? !showAuthModel.SignUpModel : showAuthModel.SignUpModel
+  };
+
+  return (
+    <Btn className={className} onClick={() => setShowAuthModel(setter)}>
+      {name}
+    </Btn>
+  );
+}
---
+++ src/Layout/Signup/Signup.tsx
@@ -0,0 +1,130 @@
+import axiosInstance from '../../../axiosConfig';
+import React, { useState } from 'react';
+import * as M from '../../../styles/apiRes.style';
+import * as S from '../../../styles/Login.Style';
+import { useAuthValue } from '../../Context/AuthContext';
+import { Iauth as response } from '../../../interface/api';
+
+import Loader from '../../Loader/Loader';
+
+export default function Signup(): JSX.Element {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const { authStatus, setAuthStatus } = useAuthValue();
+
+  const registerUser = async (event: React.SyntheticEvent<EventTarget>): Promise<void> => {
+    event.preventDefault(); // don't redirect the page
+
+    const user = {
+      userEmail: email,
+      userPassword: password,
+      userNickname: username
+    };
+    try {
+      const res = await axiosInstance.post('/users/signup', user);
+      const data: response = res.data;
+      setIsLoading(true);
+      setAuthStatus(data);
+      console.log(data);
+      window.location.reload();
+    } catch (error) {
+      const err = await error.response.data;
+      setIsLoading(true);
+      setAuthStatus(err);
+      console.log('ERROR => ', err); // this is the main part. Use the response property from the error object
+      return error.response;
+    }
+  };
+
+  console.log(`${username} + ${email} + ${password} ${process.env.API}`);
+
+  const message = authStatus?.message?.replace(/\{|\}/gi, '');
+  console.log(message);
+
+  return (
+    <S.Wrapper>
+      {isLoading ? (
+        authStatus?.status === 'sucess' ? (
+          <M.Message status={authStatus?.status}>{authStatus?.message}</M.Message>
+        ) : authStatus?.status === 'fail' ? (
+          <M.Message status={authStatus?.status}>{authStatus?.message}</M.Message>
+        ) : (
+          ''
+        )
+      ) : (
+        <Loader />
+      )}
+
+      <S.FormWrapper>
+        <h2 className='heading-secondary ma-bt-lg'>Create your account!</h2>
+        <div className='login__line'>
+          <div className='login__text'>Already a user?</div>
+          <a className='login__link' href='#'>
+            Login now
+          </a>
+        </div>
+        <form className='form form--signup' onSubmit={registerUser}>
+          <S.FormGroup>
+            <S.FormGroupLable>Your name</S.FormGroupLable>
+            <S.FormGroupInput
+              className='form__input'
+              id='name'
+              type='text'
+              placeholder=''
+              required
+              onChange={e => setUsername(e.target.value)}
+            />
+          </S.FormGroup>
+          <S.FormGroup>
+            <S.FormGroupLable>Email address</S.FormGroupLable>
+            <S.FormGroupInput
+              className='form__input'
+              id='email'
+              type='email'
+              placeholder='you@example.com'
+              required
+              onChange={e => setEmail(e.target.value)}
+            />
+          </S.FormGroup>
+          <S.FormGroup>
+            <S.FormGroupLable>Password</S.FormGroupLable>
+            <S.Warning color={password !== confirmPassword ? '#e24646' : '#6ed76e'}>
+              {password !== confirmPassword && 'Password mismatch'}
+            </S.Warning>
+            <S.FormGroupInput
+              className='form__input'
+              id='password'
+              type='password'
+              placeholder='••••••••'
+              required
+              onChange={e => setPassword(e.target.value)}
+            />
+          </S.FormGroup>
+          <S.FormGroup>
+            <S.FormGroupLable>Confirm password</S.FormGroupLable>
+            <S.Warning color={password !== confirmPassword ? '#d14646' : '#6ed76e'}>
+              {password !== confirmPassword && 'Password mismatch'}
+            </S.Warning>
+            <S.FormGroupInput
+              className='form__input'
+              id='passwordConfirm'
+              type='password'
+              placeholder='••••••••'
+              required
+              onChange={e => setConfirmPassword(e.target.value)}
+            />
+          </S.FormGroup>
+
+          <div className='form__group'>
+            <S.button color='#6c5dd3' onClick={() => setIsLoading(false)}>
+              {`Sign up ${password === confirmPassword ? '👍' : '😫'}`}
+            </S.button>
+          </div>
+        </form>
+      </S.FormWrapper>
+    </S.Wrapper>
+  );
+}
