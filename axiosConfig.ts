56dcadaf4a619831409c087b39be3a1c5ce8a59d
Ankit Singh <singhankit25apr@gmail.com>
2021-07-21 21:31:33 +0530

    Singn up component refurbished

--- axiosConfig.ts
+++ axiosConfig.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-let API;
-if (process.env.NODE_ENV.trim() === 'development') {
-  API = process.env.BASE_URL;
-} else {
-  API = process.env.PRODUCTION_URL;
-}
+// let API;
+// if (process.env.NODE_ENV.trim() === 'development') {
+//   API = process.env.BASE_URL;
+// } else {
+//   API = process.env.PRODUCTION_URL;
+// }
 
-// const axiosInstance = axios.create({
-//   baseURL: 'https://devblogapi.herokuapp.com/api/v1'
-// });
 const axiosInstance = axios.create({
-  baseURL: `${API}`
+  baseURL: 'https://devblogapi.herokuapp.com/api/v1'
 });
+// const axiosInstance = axios.create({
+//   baseURL: `${API}`
+// });
 
 axiosInstance.defaults.withCredentials = true;
 
