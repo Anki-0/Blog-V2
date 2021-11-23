d8f0e8feecb7c74437cf1b9c189219d7a98578c6
ankit-0 <singhankit25apr@gamil.com>
2021-11-23 17:26:46 +0530

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,12 +41,6 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800&display=swap'
-            rel='stylesheet'
-          />
           <link
             rel='stylesheet'
             href='https://unpkg.com/dracula-prism/dist/css/dracula-prism.css'
