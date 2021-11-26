5a24c16e6e39ff199b47064b5ef7f0b2196d8aad
ankit-0 <singhankit25apr@gamil.com>
2021-11-26 19:09:49 +0530

    .

--- axiosConfig.ts
+++ axiosConfig.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-let API;
-if (process.env.NODE_ENV.trim() === 'development') {
-  API = process.env.BASE_URL;
-} else {
-  API = process.env.BASE_URL;
-}
-const axiosInstance = axios.create({
-  baseURL: `${API}`
-});
-
+// let API;
+// if (process.env.NODE_ENV.trim() === 'development') {
+//   API = process.env.BASE_URL;
+// } else {
+//   API = process.env.BASE_URL;
+// }
 // const axiosInstance = axios.create({
-//   baseURL: 'https://devblogapi.herokuapp.com/api/v1/'
+//   baseURL: `${API}`
 // });
+
+const axiosInstance = axios.create({
+  baseURL: 'https://devblogapi.herokuapp.com/api/v1/'
+});
 
 axiosInstance.defaults.withCredentials = true;
 export default axiosInstance;
