44c00ef2ecb550eef22bf83dd5ea02f558e85687
Ankit Singh <singhankit25apr@gmail.com>
2021-11-28 00:42:40 +0530

    Update axiosConfig.ts

--- axiosConfig.ts
+++ axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let API;
+/*let API;
 if (process.env.NODE_ENV.trim() === 'development') {
   API = process.env.BASE_URL;
 } else {
@@ -8,11 +8,11 @@ if (process.env.NODE_ENV.trim() === 'development') {
 }
 const axiosInstance = axios.create({
   baseURL: `${API}`
-});
+});*/
 
-// const axiosInstance = axios.create({
-//   baseURL: 'https://devblogapi.herokuapp.com/api/v1/'
-// });
+ const axiosInstance = axios.create({
+   baseURL: 'https://devblogapi.herokuapp.com/api/v1/'
+ });
 
 axiosInstance.defaults.withCredentials = true;
 export default axiosInstance;
